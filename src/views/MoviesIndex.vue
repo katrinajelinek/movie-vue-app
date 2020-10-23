0aec8c63e79157aa7e70de4ea6076e0eebbce698
katrinajelinek <kmrjelinek@gmail.com>
2020-10-23 08:00:40 -0600

    adds sort by and order by methods

--- src/views/MoviesIndex.vue
+++ src/views/MoviesIndex.vue
@@ -2,11 +2,15 @@
   <div class="movies-index">
     <h1>Movies!</h1>
 
-    Search by title: <input v-model="titleFilter" list="titles">
-    <datalist id="titles">
-      <option v-for="movie in movies">{{movie.title}}</option>
-    </datalist>
-    <div v-for="movie in filterBy(movies, titleFilter, 'title')">
+    <div>
+      <input v-model="titleFilter" list="titles" placeholder="Search by title">
+      <datalist id="titles">
+        <option v-for="movie in movies">{{movie.title}}</option>
+      </datalist>
+      <button v-on:click="setSortAttribute('title')">Sort Alphabetically</button> <br>
+      <button v-on:click="setSortAttribute('created_at')">Sort by date</button>
+    </div>
+      <div v-for="movie in orderBy(filterBy(movies, titleFilter, 'title'), 'title')">
       <router-link :to="`/movies/${movie.id}`">
         <h2>Title: {{ movie.title }}</h2>
       </router-link>
@@ -27,11 +31,11 @@ import Vue2Filters from "vue2-filters";
 
 export default {
   mixins: [Vue2Filters.mixin],
-
   data: function() {
     return {
       movies: [],
       titleFilter: "",
+      sortAttribute: "",
     };
   },
   created: function() {
@@ -43,6 +47,9 @@ export default {
         console.log(response.data);
         this.movies = response.data;
       });
+    },
+    setSortAttribute: function (attribute) {
+      this.sortAttribute = attribute;
     }
   }
 };
