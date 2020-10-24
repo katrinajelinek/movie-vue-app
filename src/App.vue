d27e3a6bd4c73326eb2b0f6789e159857b1f15c4
katrinajelinek <kmrjelinek@gmail.com>
2020-10-23 18:09:51 -0600

    adds fancy html nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <!-- <router-link to="/about">About</router-link> -->
-      <span v-if="!isLoggedIn()">
-        <router-link to="/signup">Signup</router-link> |
-      </span>
-      <span v-if="!isLoggedIn()">
-        <router-link to="/login">Login</router-link> |
-      </span>
-      <span v-if="isLoggedIn()">
-        <router-link to="/logout">Logout</router-link> |
-      </span>
-      <router-link to="/movies">Movies</router-link> |
-      <router-link to="/movies/new">New Movie</router-link> |
-    </div>
+    <nav class="main-nav overlay clearfix">
+      <ul id="menu">
+        <li class="nav-home nav-current" role="presentation">
+          <router-link to="/movies">Movies</router-link>
+        </li>
+        <li class="nav-about-us" role="presentation" v-if="!isLoggedIn()">
+          <router-link to="/signup">Signup</router-link>
+        </li>
+        <li class="nav-author-page" role="presentation" v-if="!isLoggedIn()">
+          <router-link to="/login">Login</router-link>
+        </li>
+        <li class="nav-author-page" role="presentation" v-if="isLoggedIn()">
+          <router-link to="/logout">Logout</router-link>
+        </li>
+      </ul>
+    </nav>
     <router-view/>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
 
 <script>
 
